162b3a0bc7fe277bc28886ef45789ab5f68a498b
will <will1996@terpmail.umd.edu>
2017-09-05 15:44:14 -0400

    work from 09-05-2017

--- CNAMsite/CNAMsite/settings.py
+++ CNAMsite/CNAMsite/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'EquipmentSchedule.apps.EquipmentscheduleConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'EquipmentSchedule.apps.EquipmentscheduleConfig'
 ]
 
 MIDDLEWARE = [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'EST'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
